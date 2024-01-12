8e7d1cfb9019956d5d8694723233bcd09a22f55a
hancher <hanchers@outlook.com>
2024-01-12 21:31:24 +0800

    配置默认为空的情况

--- web-demo/src/main/java/com/hancher/demo/webdemo/controller/TestController.java
+++ web-demo/src/main/java/com/hancher/demo/webdemo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.hancher.demo.webdemo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +14,23 @@ import java.util.Optional;
 @Slf4j
 public class TestController {
 
+    @Value("${hancher.test.empty}")
+    private String emptyCofig;
+
     // 单纯的同步类
     @GetMapping("hello")
+
     public String hello(@RequestParam("redirect") String redirectUrl) throws InterruptedException {
         String s = Optional.ofNullable(redirectUrl).orElse("no url");
         log.info(s);
         return s;
     }
+
+    // 单纯的同步类
+    @GetMapping("config")
+    public String config() throws InterruptedException {
+        // ${hancher.test.empty} + 配置只要存在，以配置为准。hancher.test.empty= 为空字符串。 没有配置报错
+        return "config="+emptyCofig;
+    }
+
 }
